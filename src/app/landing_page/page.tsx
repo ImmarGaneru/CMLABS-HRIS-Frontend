327f5bcac30d1b4a9ee68c21802520f319e0cc25
Yayun Eldina <yayuneldina02@gmail.com>
2025-04-30 17:37:36 +0700

    Slicing Landing Page new

---
+++ src/app/landing_page/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from './heroSection';
+import Features from './features';
+import FAQ from './faq';
+import CTA from './cta';
+
+export default function LandingPage() {
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <FAQ />
+      <CTA />
+    </>
+  );
+}
