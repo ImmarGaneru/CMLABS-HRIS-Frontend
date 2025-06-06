5926c2e62664a4907a0952b3cab044c8a95ec311
hoshigakikisame <rianatgithub@gmail.com>
2025-06-06 19:06:15 +0700

    ci: disabling strict eslint rules; tidy up docker configs.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,7 +14,10 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-wrapper-object-types': 'off'
+      '@typescript-eslint/no-wrapper-object-types': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      
     }
   })
 ];
